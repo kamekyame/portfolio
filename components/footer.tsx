237e49d4ce0c55e697519c98e52462b9946669a0
Suzu Tomo <kamekyame@outlook.com>
2022-01-04 01:59:50 +0900

    feat(footer): GithubとTwitterへのリンクを追加

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,14 +1,44 @@
-import { styled } from "@mui/material/styles";
-
-const Footer = styled("footer")(({ theme }) => ({
-  backgroundColor: theme.palette.primary.main,
-  display: "flex",
-  justifyContent: "center",
-}))
+import { Box, Typography } from "@mui/material";
+import { Twitter, GitHub } from "@mui/icons-material";
 
 export default function App() {
-  return <Footer>
-    <p>@2022 kamekyame</p>
-  </Footer>
-
+  return (
+    <Box
+      component="footer"
+      sx={(theme) => ({
+        backgroundColor: theme.palette.primary.main,
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        py: 2,
+      })}
+    >
+      <Box
+        sx={{
+          m: 1,
+          "& a": { mx: 2 },
+        }}
+      >
+        <Box
+          component="a"
+          href="https://github.com/kamekyame"
+          target="_blank"
+          color="inherit"
+        >
+          <GitHub />
+        </Box>
+        <Box
+          component="a"
+          href="https://twitter.com/SuzuTomo2001"
+          target="_blank"
+          color="inherit"
+        >
+          <Twitter />
+        </Box>
+      </Box>
+      <Box>
+        <Typography>@2022 kamekyame</Typography>
+      </Box>
+    </Box>
+  );
 }
