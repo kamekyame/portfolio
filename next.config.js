84855beb41d910fb633bf1825d1efdb646692595
SuzuTomo <47011206+kamekyame@users.noreply.github.com>
2022-08-27 01:48:11 +0900

    fix: inline-svgをBabelではなくsvgr/webpackを使用するように変更 (#15)

--- next.config.js
+++ next.config.js
@@ -3,5 +3,13 @@ module.exports = {
   reactStrictMode: true,
   images: {
     domains: ["images.microcms-assets.io"]
+  },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      use: ['@svgr/webpack'],
+    })
+    return config;
   }
 }
