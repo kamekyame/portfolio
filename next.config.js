67c7d93e8bed25a46e8e5c488144f0e94414f636
SuzuTomo <47011206+kamekyame@users.noreply.github.com>
2022-09-23 23:30:20 +0900

    feat(t7s/resume): ナナシス履歴書ギャラリーを追加 (#19)
    * feat(t7s/resume): ナナシス履歴書ギャラリーを追加
    
    * fix: 不要なconsole.logを削除
    
    * fix(contents): ナナシス履歴書ギャラリーへのリンクを追加

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 module.exports = {
   reactStrictMode: true,
   images: {
-    domains: ["images.microcms-assets.io"]
+    domains: ["images.microcms-assets.io", "pbs.twimg.com"]
   },
   webpack(config) {
     config.module.rules.push({
